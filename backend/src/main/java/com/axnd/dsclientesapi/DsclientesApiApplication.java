31575b455ba0c584ec2547fc2337818cb331bbe6
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-03-09 18:40:12 +0100

    Backend project created

---
+++ backend/src/main/java/com/axnd/dsclientesapi/DsclientesApiApplication.java
@@ -0,0 +1,13 @@
+package com.axnd.dsclientesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientesApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/axnd/dsclientesapi/DsclientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axnd.dsclientesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
