25361d8294080b93d12041b823346b62580031f6
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-03-26 02:12:15 +0100

    class anotation added

--- backend/src/main/java/com/axnd/dsclientesapi/entities/Client.java
+++ backend/src/main/java/com/axnd/dsclientesapi/entities/Client.java
@@ -3,14 +3,28 @@ package com.axnd.dsclientesapi.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable {
     private static final long serialVersionUID = 1;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
     private Double income;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
+
     private Integer children;
 
     public Client() {
