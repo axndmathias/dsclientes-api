ee65dc6388fcf7e62117805034a505b33ea85128
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-03-28 23:43:46 +0200

    Database added seeding added

--- backend/src/main/java/com/axnd/dsclientesapi/entities/Client.java
+++ backend/src/main/java/com/axnd/dsclientesapi/entities/Client.java
@@ -3,6 +3,7 @@ package com.axnd.dsclientesapi.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,7 +22,7 @@ public class Client implements Serializable {
     private String cpf;
     private Double income;
 
-    // @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
 
     private Integer children;
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name) VALUES ('Joao das coves');
+INSERT INTO tb_client (name) VALUES ('maria da feira');
+INSERT INTO tb_client (name) VALUES ('ze do peixe');
