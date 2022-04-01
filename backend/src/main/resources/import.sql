16f668b3ea6e1bafb769c18e3426f05add569cf9
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-04-01 23:01:48 +0200

    import.sql file updated

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,7 +1,3 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('Elton felix', '14222960675', 1220.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('Maria rocha', '48252330827', 2000.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('Fernanda santos', '86613637300', 2200.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('Mario bezerra', '35524565460', 3500.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 3);
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('Nélio alves', '84463863562', 1220.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('Naruto uzumaki', '14443738703', 4000.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES('David wilian', '24056378479', 2500.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 5);
